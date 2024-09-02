a9b0be561dca37b40a014ff0c702acd98a6595d1
RFuentesA <159492816+RFuentesA@users.noreply.github.com>
2024-09-02 07:50:57 -0500

    clase alojamiento

---
+++ SRC/gestorAplicacion/alojamiento/Alojamiento.py
@@ -0,0 +1,12 @@
+class Alojamiento():
+    #ATRIBUTOS
+    def __init__(self, nombre, locacion, precioDia, estrellas):
+        self._nombre = nombre
+        self._locacion = locacion
+        self._precioDia = precioDia
+        self._estrellas = estrellas
+        #De clase 
+        self._alojamientos = []
+
+    def calcularPrecio(self, dias):
+        return dias * (this._precioDia)
