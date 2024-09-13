fa7eb3c23c2db01621deebd093654e64f0ba64ea
Richi <159492816+RFuentesA@users.noreply.github.com>
2024-09-12 19:19:55 -0500

    ..

--- SRC/gestorAplicacion/alojamiento/Alojamiento.py
+++ SRC/gestorAplicacion/alojamiento/Alojamiento.py
@@ -1,12 +1,12 @@
 class Alojamiento():
     #ATRIBUTOS
+    alojamientos = []
+    
     def __init__(self, nombre, locacion, precioDia, estrellas):
         self._nombre = nombre
         self._locacion = locacion
         self._precioDia = precioDia
         self._estrellas = estrellas
-        #De clase 
-        self._alojamientos = []
 
     def calcularPrecio(self, dias):
         return dias * (self._precioDia)
