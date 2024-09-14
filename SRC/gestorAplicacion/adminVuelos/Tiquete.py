c21f5eb80410342686082f7a444b6b0b21343ef8
Richi <159492816+RFuentesA@users.noreply.github.com>
2024-09-13 22:42:12 -0500

    Update Tiquete

--- SRC/gestorAplicacion/adminVuelos/Tiquete.py
+++ SRC/gestorAplicacion/adminVuelos/Tiquete.py
@@ -1,106 +1,100 @@
-#Autores: Ricardo Fuentes, Valery Fernandez, Juan Luis Sucerquia, Mariana Sanchez, Jose Forero
-from gestorAplicacion.alojamiento.Alojamiento import Alojamiento
-from gestorAplicacion.adminVuelos.Pasajero import Pasajero
-from gestorAplicacion.adminVuelos.Vuelo import Vuelo
+#Autores: Ricardo Fuentes, Valery Fernandez, Juan Luis Sucerquia, Mariana Sanchez
+from tkinter.messagebox import NO
+from gestorAplicacion.hangar.Aeronave import Aeronave
+from gestorAplicacion.hangar.Avion import Avion
+from gestorAplicacion.hangar.Avioneta import Avioneta
+from gestorAplicacion.hangar.Clase import Clase
 from gestorAplicacion.hangar.Silla import Silla
+from gestorAplicacion.hangar.Ubicacion import Ubicacion
+from gestorAplicacion.alojamiento.Alojamiento import Alojamiento
 
 #CLASE TIQUETE CONTIENE LA INFORMACION ASOCIADA AL VUELO, SILLA Y ALOJAMIENTOS SELECCIONADOS
 #TAMBIEN CONTIENE UN ID PARA TIQUETE Y EL VALOR DEL PRECIO.
 #SUS METODOS NOS PERMITEN CONSOLIDAR EL RESUMEN DEL TIQUETE QUE TIENE UN PASAJERO ASOCIADO A UN VUELO
 class Tiquete:
 
-    #ATRIBUTOS
+    #CONSTRUCTOR
     def __init__(self, id, precio, vuelo):
         self._id = id
         self._precio = precio
         self._vuelo = vuelo
-        vuelo.get_tiquetes().append(self)
+        self._silla = None
+        self._pasajero = None
+        self._alojamiento = None
+        vuelo.getTiquetes().append(self)
 
-    def __init__(self, id, precio, vuelo, silla, pasajero, alojamiento):
-        self._id = id
-        self._precio = precio
-        self._vuelo = vuelo
-        self._silla = silla
-        self._pasajero = pasajero
-        self._alojamiento = alojamiento
-        vuelo.get_tiquetes().append(self)
+    # METODOS
 
-    #METODOS
-
-    #ESTE METODO NO TIENE PARAMETROS DE ENTRADA PERO RETORNA UN BOOLEANO, YA QUE 
-	#SU OBJETIVO ES ASIGNARLE EL PRECIO A CADA INSTANCIA DE TIQUETE OBTENIENDO LOS PRECIOS
-	#DEL VUELO Y SILLA SELECCIONADOS CON UN DESCUENTO POR CONFORME AL ATRIBUTO EDAD DEL PASAJERO
-    def asignar_precio():
-        precio_total = _vuelo.get_precio() + self.get_silla().getClase().get_precio()
-        if (pasajero.get_edad()<5):
+    # Este metodo no tiene parametros de entra o de salida porque el valor resultante
+    # es guardado en el atributo precio de cada instancia
+    def asignarPrecio(self,*args):
+        hayDescuento = False
+        precio_total =self._vuelo.getPrecio() + self.getSilla().getClase().value
+        if self._pasajero.getEdad()<5:
             hayDescuento = True
-            self._precio = (precio_total - (precio_total*0.25))
-
-        elif(pasajero.get_edad()>5 and pasajero.get_edad()<=10):
-            self._precio = (precio_total - (precio_total*0.15))
+            self._precio = int((precio_total - (precio_total*0.25)))
+        elif self._pasajero.getEdad()>5 and self._pasajero.getEdad()<=10:
+            self._precio = int((precio_total - (precio_total*0.15)))
             hayDescuento = True
         else:
             self._precio = precio_total
+
+        if len(args) != 0 :
+
+            self._precio += self._alojamiento.calcularPrecio(args[0]) 
+
         return hayDescuento
-    
-    #ESTE METODO SOBRECARGA EL METODO ASIGNAR PRECIO RECIBIENDO COMO PARAMETRO DE ENTRADA
-	#UN ENTERO Y SU RETORNO ES VACIO. SU FUNCION SUMARLE A CADA INSTANCIA DE TIQUETE EL PRECIO DEL ALOJAMIENTO
-	#SELECCIONADO DE ACUERDO AL NUMERO DE DIAS INGRESADO JUNTO A LOS PRECIOS DEL VUELO Y SILLA
-    def asignar_precio(num_dias):
-        precio_total = Vuelo.get_precio() + Alojamiento.calcular_precio(num_dias) + self.get_silla().getClase().get_precio()
-        if (pasajero.get_edad() < 5):
-            self._precio = (precio_total - (precio_total*0.15))
-        elif (_pasajero.get_edad()>5 and _pasajero.get_edad()<=10):
-            self._precio = (precio_total - (precio_total*0.15))
-        else:
-            self._precio = precio_total
 
     #EL METODO AGREGAR LA INSTANCIA DE TIQUETE CREADA AL ARRAY
     #DE TIQUETES QUE TIENE ASOCIADO AL VUELO SELCCIONADO, POR TAL RAZON NO RECIBE PARAMETRO Y SU
 	#RETORNO ES VACIO
-    def confirmar_compra():
-        self._vuelo.get_tiquetes().append(self)
+    def confimarCompra(self):
+        self._vuelo.getTiquetes().append(self)
 
-    #Espacio para devolver a la interfaz el recibo 
+    def __str__(self):
+        if self._alojamiento is None:
+            return "*************************************\n"+ "      Su compra ha sido exitosa\n"+ "    Gracias por confiar en nostros\n"+ "*************************************\n"+ "------------------------------------\n"+ "      Tiquete No."+ str(self._id) + "\n"+ "------------------------------------\n"+ "Nombre Pasajero: " + self._pasajero.nombre + "\n" + "Fecha: " + self._vuelo.getFecha_de_salida() + "\n" + "Vuelo: " + str(self._vuelo.getID()) + "\n" + "Num Silla: " + str(self._silla.getNumero_de_silla()) + " " + self._silla.getUbicacion().value + "\n" + "Origen: " + self._vuelo.getOrigen() + "\n" + "Destino: " + self._vuelo.getDestino() + "\n" + "Precio Total: " + str(self.getPrecio()) + "\n" + "------------------------------------\n"
 
 
+        else:
 
-    #SETTER Y GETTER
-    def set_id(self, id):
-        self._id = id
+            return "*************************************\n"+ "      Su compra ha sido exitosa\n"+ "    Gracias por confiar en nostros\n"+ "*************************************\n"+ "------------------------------------\n"+ "      Tiquete No."+ str(self._id) + "\n"+ "------------------------------------\n"+ "Nombre Pasajero: " + self._pasajero.nombre + "\n" + "Fecha: " + self._vuelo.getFecha_de_salida() + "\n" + "Vuelo: " +str(self._vuelo.getID()) + "\n" + "Silla: " + str(self._silla.getNumero_de_silla()) + " - " + self._silla.getUbicacion().value + "\n" + "Origen: " + self._vuelo.getOrigen() + "\n" + "Destino: " + self._vuelo.getDestino() + "\n" + "Alojamiento: " + self._alojamiento.getNombre() + "\n" + "Precio Total: " + str(self.getPrecio()) + "\n" + "------------------------------------\n"
 
-    def get_id(self):
+
+    #GETTERS Y SETTERS
+    def getId(self):
         return self._id
 
-    def set_precio(self, precio):
-        self._precio = precio
-    
-    def get_precio(self):
+    def setId(self, id):
+        self._id = id
+
+    def getPrecio(self):
         return self._precio
 
-    def set_vuelo(self, vuelo):
+    def setPrecio(self, precio):
+        self._precio = precio
+
+    def getVuelo(self):
+        return self._vuelo
+
+    def setVuelo(self, vuelo):
         self._vuelo = vuelo
 
-    def get_vuelo(self):
-        return self._vuelo 
-    
-    def set_silla(self, silla):
+    def getSilla(self):
+        return self._silla
+
+    def setSilla(self, silla):
         self._silla = silla
         silla.setEstado(False)
-    
-    def get_silla(self):
-        return self._silla
-    
-    def set_pasajero(self, pasajero):
-        self._pasajero = pasajero
 
-    def get_pasajero(self):
+    def getPasajero(self):
         return self._pasajero
 
-    def set_alojamiento(self, alojamiento):
-        self._alojamiento = alojamiento
-    
-    def get_alojamiento(self):
-        return self._alojamiento
+    def setPasajero(self, pasajero):
+        self._pasajero = pasajero
 
-    
+    def getAlojamiento(self):
+        return self._alojamiento
+        
+    def setAlojamiento(self, alojamiento):
+        self._alojamiento = alojamiento
