4aac3b0777455eca680d274ad45d58a5e26e9b38
Valeryfer <vafernandez@unal.edu.co>
2024-09-14 21:35:25 -0500

    VentanaPrincipal

--- SRC/VentanaPrincipal.py
+++ SRC/VentanaPrincipal.py
@@ -75,3 +75,80 @@ class VentanaSecundaria(Toplevel):
 
         self.label_descripcion = Label(self.frame_descripcion, text = "Realiza reservaciones de vuelos y alojamientos, y mantén la informacion actualizada de vuelos, aviones y alojamientos", font = ("Segoe UI", 10), bg="gray90")
         self.label_descripcion.pack(ipadx = 2, ipady = 2, padx = 5, pady= 5)
+
+        self.labelTexto = Label(self.ventana_operaciones, text = "Puedes hacerlo con las acciones dispuestas en el menu <Procesos y consultas>", font = ("Segoe UI", 10))
+        self.labelInicio = Label(self.ventana_operaciones)
+        self.imagenInicio = PhotoImage(file = 'SRC/imagenes/Reservacion.png')
+        self.labelInicio["image"] = self.imagenInicio
+        self.labelTexto.pack(ipadx = 10, ipady =10, padx = 10, pady= 10)
+        self.labelInicio.pack(ipadx = 10, ipady =10, padx = 10, pady= 10)
+        #FIN ZONA DE Labels
+
+        self.contador_mostrarVuelosPorAerolineas = 0
+        self.__class__.en_uso = True
+
+    #--------------------------------------------------------------------------------------------------------------
+    #Se despliega un Message Box con la informacion basica de lo que hace la aplicacion.
+    def descripcionApp(self):
+        descripcion = messagebox.showinfo(title = "Informacion", message = "SISTEMA DE RESERVA DE VUELOS",
+        detail = "La aplicacion permite hacer reservaciones de un vuelo y un alojamiento en el lugar de destino, ademas de algunas opciones de administrador.")
+
+    #--------------------------------------------------------------------------------------------------------------
+    # Retorna a la Ventana de Inicio del programa.
+    def salirVentana(self):
+        self.__class__.en_uso = False
+        self.ventanaInicio.deiconify()
+        return super().destroy()
+
+    #---------------------------------------------------------------------------------------------------------------------------------------
+    # Muestra los vuelos disponibles por aerolinea, cada una en un frame que se actualiza por otro cada vez que se presiona el botón siguiente
+
+    def mostrarVuelosPorAerolineas(self):
+        self.label_proceso.config(text = "Vuelos disponibles por aerolínea")
+        self.label_descripcion.config(text = "Aquí puede visualizar los vuelos que están disponibles por nuestrar aerolíneas")
+        self.ventana_operaciones.pack_forget()
+        self.ventana_operaciones= Frame(self.frame,relief="groove",bd=2)
+        self.ventana_operaciones.pack(ipadx = 2, ipady =2, padx = 2, pady= 2,fill=X)
+
+        # Se ejecuta cada vez que se presiona el boton "siguiente", para reemplazar el label mostrado por pantalla
+        def siguiente():
+            self.contador_mostrarVuelosPorAerolineas +=1
+            if self.contador_mostrarVuelosPorAerolineas == len(lista_labels):
+                self.contador_mostrarVuelosPorAerolineas =0
+            lista_labels[self.contador_mostrarVuelosPorAerolineas-1].pack_forget()
+            lista_labels[self.contador_mostrarVuelosPorAerolineas].pack()
+
+        boton_siguiente = Button(self.ventana_operaciones,text= "Siguiente",command=siguiente)
+        boton_siguiente.pack()
+        lista_labels=Admin.mostrarVuelosPorAerolineas(self.ventana_operaciones)
+        lista_labels[0].pack()
+
+    #-------------------------------------------------------------------------------------------------------------------------------------
+    # Funcion auxiliar que permite mostrar una lista de vuelos disponibles por cada aerolinea hacia un destino seleccionado por el usuario
+    # cada aerolinea en un frame que se actualiza por otro cada vez que se presiona el botón siguiente, y continuar con la compra de un
+    # tiquete
+
+    def buscarVuelos(self,formulario):
+        try:
+            hay_excepcion = formulario.aceptar()
+        except ExcepcionStringNumero as owo:
+            messagebox.showerror(title="Error",message=owo.mensaje_error_inicio)
+            # self.generarTiquete()
+            return
+        if hay_excepcion:
+            self.generarTiquete()
+            return
+
+        self.label_proceso.config(text = "Vuelos disponibles")
+        self.label_descripcion.config(text = "Lista los vuelos disponibles de acuerdo a los parámetros ingresados")
+
+        self.ventana_operaciones.pack_forget()
+
+        hayFecha = False
+        fecha = None
+        destino = formulario.valor_entradas[0]
+        lista_general = None
+
+        if len(formulario.valor_entradas) == 2:
+            hayFecha = True
+            fecha = formulario.valor_entradas[1]
